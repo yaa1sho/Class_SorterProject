5dd38f4892b1f51aab63866574de272abc6bf4c3
galexal <galexal@yandex.ru>
2024-09-23 11:56:36 +0400

    реализованы методы получения объектов Книга из разных источников

--- src/main/java/org/example/classes/Book.java
+++ src/main/java/org/example/classes/Book.java
@@ -2,6 +2,10 @@ package main.java.org.example.classes;
 
 import main.java.org.example.input.IBuilder;
 
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Book {
     private String name;
     private String author;
@@ -19,9 +23,10 @@ public class Book {
         return name;
     }
 
-    private Book(){}
+    private Book() {
+    }
 
-    private Book(Builder builder){
+    private Book(Builder builder) {
         name = builder.name;
         author = builder.author;
         pageCount = builder.pageCount;
@@ -33,9 +38,11 @@ public class Book {
         private int pageCount;
 
 
-        public Builder(){}
+        public Builder() {
+        }
 
-        public Builder(String model){}
+        public Builder(String model) {
+        }
 
         public Builder setName(String name) {
             this.name = name;
@@ -52,26 +59,76 @@ public class Book {
             return this;
         }
 
-        public Book build(){
+        public Book build() {
             return new Book(this);
         }
 
         @Override
         public Book buildFromString(String line) {
-            //TODO реализовать метод
+            String[] values = line.split(",");
+            setName(values[0].trim());
+            setAuthor(values[1].trim());
+            int pageCount = -1;
+            try {
+                pageCount = Integer.parseInt(values[2].trim());
+            } catch (NumberFormatException ignored) {
+            }
+            if (pageCount > 0) {
+                setPageCount(pageCount);
+                return new Book(this);
+            }
             return null;
         }
 
         @Override
         public Book buildFromConsole() {
-            //TODO реализовать метод
-            return null;
+            Scanner scanner = new Scanner(System.in);
+            String input;
+            do {
+                System.out.println("Введите название книги или exit:");
+                input = scanner.nextLine();
+                if ("exit".equalsIgnoreCase(input)) {
+                    return null;
+                }
+            } while (input.isEmpty() || input.isBlank());
+            setName(input);
+            do {
+                System.out.println("Введите фамилию автора или exit:");
+                input = scanner.nextLine();
+                if ("exit".equalsIgnoreCase(input)) {
+                    return null;
+                }
+            } while (input.isEmpty() || input.isBlank());
+            setAuthor(input);
+            int pageCount = -1;
+            do {
+                System.out.println("Введите количество страниц или exit:");
+                input = scanner.nextLine();
+                if ("exit".equalsIgnoreCase(author)) {
+                    return null;
+                }
+                try {
+                    pageCount = Integer.parseInt(input.trim());
+                } catch (NumberFormatException exception) {
+                    System.out.println("Количество страниц должно быть целым положительным числом!");
+                }
+            } while (pageCount < 1);
+            setPageCount(pageCount);
+            return new Book(this);
         }
 
         @Override
         public Book buildRandomObj() {
-            //TODO реализовать метод
-            return null;
+            List<String> names = List.of("Война и мир", "Преступление и наказание", "Гарри Поттер и философский камень",
+                    "1984", "Убить пересмешника", "Мастер и Маргарита", "Сто лет одиночества", "Ловец в ржи",
+                    "До встречи с тобой", "Анна Каренина");
+            List<String> authors = List.of("Достоевский", "Толстой", "Набоков", "Пушкин", "Маяковский", "Булгаков",
+                    "Тургенев", "Ахматова", "Чехов", "Гоголь");
+            Random random = new Random();
+            setName(names.get(random.nextInt(names.size())));
+            setAuthor(authors.get(random.nextInt(authors.size())));
+            setPageCount(random.nextInt(3599) + 1);
+            return new Book(this);
         }
     }
 
